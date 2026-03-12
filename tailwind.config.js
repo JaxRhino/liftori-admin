8d0e861f6c22da22d69e1548668f17849dff1caa
JaxRhino <rhinomarch78@gmail.com>
2026-03-12 13:24:54 -0400

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        navy: {
+          900: '#060B18',
+          800: '#0A1628',
+          700: '#0F1D35',
+          600: '#152642',
+          500: '#1B3050'
+        },
+        brand: {
+          blue: '#0EA5E9',
+          light: '#7DD3FC',
+          ice: '#E0F7FF',
+          cyan: '#00E5CC'
+        }
+      },
+      fontFamily: {
+        sans: ['DM Sans', 'system-ui', 'sans-serif'],
+        mono: ['DM Mono', 'monospace'],
+        display: ['Bebas Neue', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
+}
