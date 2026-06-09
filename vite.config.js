dc4617db4c9e157f717cec2089a23b961e8de2c4
Ryan March <rhinomarch78@gmail.com>
2026-06-09 19:00:44 -0400

    Fix Excalidraw process.env crash in local dev

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  define: {
+    'process.env.IS_PREACT': JSON.stringify('false'),
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true
